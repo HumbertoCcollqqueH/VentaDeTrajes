93a76f6a8df6fa5f2d18f2a10b1eed7ae9eb53e1
HumbertoCcollqqueH <humber23ch@gmail.com>
2024-06-26 23:12:45 -0500

    Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\ClienteMain;
+use App\Livewire\Dashboard\Main;
 use App\Livewire\ProveedorMain;
 use App\Livewire\TrajeMain;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +15,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard',Main::class)->name('dashboard');
     Route::get('/clientes',ClienteMain::class)->name('clientes');
     Route::get('/proveedores',ProveedorMain::class)->name('proveedores');
     Route::get('/trajes',TrajeMain::class)->name('trajes');
